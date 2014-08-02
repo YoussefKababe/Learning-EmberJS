4e6e5e609d939f230b99a941c7cca71badce23fc
Youssef Kababe <youssef.kbe@gmail.com>
2014-08-02 16:38:18 +0100

    display number of incomplete todos

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -14,7 +14,16 @@ Todos.TodosController = Ember.ArrayController.extend({
 
 			todo.save();
 		}
-	}
+	},
+
+	remaining: function() {
+		return this.filterBy('isCompleted', false).get('length');
+	}.property('@each.isCompleted'),
+
+	inflection: function() {
+		var remaining = this.get('remaining');
+		return remaining === 1 ? 'todo' : 'todos';
+	}.property('remaining')
 });
 
 Todos.TodoController = Ember.ObjectController.extend({
